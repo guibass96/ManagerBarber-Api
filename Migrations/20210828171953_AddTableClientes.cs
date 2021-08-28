b052a95b816f941872046ac8a723b1292916d688
guibass96 <55796668+guibass96@users.noreply.github.com>
2021-08-28 15:09:57 -0300

    fdfd

---
+++ Migrations/20210828171953_AddTableClientes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ManangerBarberApi.Migrations
+{
+    public partial class AddTableClientes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CPF",
+                table: "Cliente",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataNascimento",
+                table: "Cliente",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Cliente",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telefone",
+                table: "Cliente",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CPF",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "DataNascimento",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "Telefone",
+                table: "Cliente");
+        }
+    }
+}
--- Models/Cliente.cs
+++
@@ -1,9 +0,0 @@
-namespace ManagerBarber_Api.Models
-{
-    public class Cliente
-    {
-         public int Id {get;set;}
-
-        public string Nome {get;set;}
-    }
-}
